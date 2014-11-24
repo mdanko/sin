51596c9d5b624032b18352e190e2c54e01aaf95d
Matej <petrigac@matcomp.cz>
2014-11-25 00:51:25 +0100

    pridany riadic

---
+++ Riadic.java
@@ -0,0 +1,11 @@
+package crossroads;
+
+/**
+ * Created by Matej on 25. 11. 2014.
+ */
+public class Riadic {
+    public Controller c;
+    public Riadic(Controller c){
+        this.c = c;
+    }
+}
